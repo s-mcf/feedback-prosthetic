84c726868e0fa368e384014adb179d9477c1fa8c
Brenden Hatton <flyboy92@gmail.com>
2014-10-18 15:11:06 -0700

    Verification of pressure sensor data
    Foam based pressure sensor analog read working and gives usable data.

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -1,7 +1,11 @@
+// HWeekend 2014 - Feedback for augmented prostetic limb
+
+
 #define GREEN 14
 #define BLUE 15
 #define RED 12
 #define delayTime 20
+#define pres_thres 150
 
 void setup()
 {                
@@ -15,6 +19,7 @@ void setup()
 }
 
 float val;
+float val2;
 
 void loop(){
   val = max(min((analogRead(1) - 700.0f) * 10.0f, 250.0f), 0);
@@ -22,6 +27,12 @@ void loop(){
   Serial.println(val);
   analogWrite(RED, 255 - val);
   analogWrite(BLUE, val);
+  
+  //this section is for reading the analog pressure sensor
+  val2 = analogRead(5);
+  Serial.print("analog 2 is: ");
+  Serial.println(val2);
   delay(10);
+  
 }
 
