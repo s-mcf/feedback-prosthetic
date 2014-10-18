4cc88216b0163c010e54551fad63421c91c07f77
Simon McFarlane <simon@piston.pw>
2014-10-18 14:53:56 -0700

    Add inital RGB LED code

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -1,15 +1,26 @@
+#define GREEN 14
+#define BLUE 15
+#define RED 12
+#define delayTime 20
+
 void setup()
 {                
   Serial.begin(38400);
+    pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(RED, OUTPUT);
+  digitalWrite(GREEN, HIGH);
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(RED, HIGH);
 }
 
-int val;
+float val;
 
-void loop()                     
-{
-  val = analogRead(1);
+void loop(){
+  val = min((analogRead(1) - 700.0f) * 10.0f, 250.0f);
   Serial.print("analog 1 is: ");
   Serial.println(val);
-  delay(200);
+  analogWrite(RED, val);
+  delay(10);
 }
 
