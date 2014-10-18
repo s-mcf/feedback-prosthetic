166fe6403737d695d6490e237da10c606ffdf07e
Simon McFarlane <simon@piston.pw>
2014-10-18 15:37:56 -0700

    Add motor and pressure pins as #DEFINEs

--- teensy/teensy.ino
+++ teensy/teensy.ino
@@ -4,6 +4,8 @@
 #define GREEN 14
 #define BLUE 15
 #define RED 12
+#define MOTOR 10
+#define PRES_PIN 5
 #define delayTime 20
 #define pres_thres 150
 
@@ -23,15 +25,17 @@ float pres;
 
 void loop(){
   temp = max(min((analogRead(1) - 700.0f) * 10.0f, 250.0f), 0);
-  Serial.print("analog 1 is: ");
+  Serial.print("temp: ");
   Serial.println(temp);
   analogWrite(RED, 255 - temp);
   analogWrite(BLUE, temp);
-  
+
   //this section is for reading the analog pressure sensor
-  pres = analogRead(5);
-  Serial.print("analog 2 is: ");
+  pres = analogRead(PRES_PIN);
+  Serial.print("pres: ");
   Serial.println(pres);
+  analogWrite(MOTOR, pres);
+
   delay(delayTime);
   
 }
